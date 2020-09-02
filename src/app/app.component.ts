712d32ac570c61ce0ac0a88f46e85a430efb1b2e
Lubovin Vyacheslav <VLubovin@ads.energoprom.ru>
2020-09-02 23:42:46 +0300

    FIrst push

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NbIconModule } from '@nebular/theme';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'my-new-project';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbCardModule, NbMenuModule } from '@nebular/theme';
+import { NbEvaIconsModule  } from '@nebular/eva-icons';
+import { NbMenuServiceComponent } from './nb-menu-service/nb-menu-service.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NbMenuServiceComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'dark' }),
+    NbMenuModule.forRoot(),
+    NbCardModule,
+    NbLayoutModule,
+    NbEvaIconsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
